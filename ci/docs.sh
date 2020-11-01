f2a4b8a9f8c98e7d19ac3887c3e2f1b4fc6fd85d
german gomez <germangb42@gmail.com>
2020-11-01 12:56:52 +0100

    ci

---
+++ ci/docs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOCS_DIR=pages_deploy/
+
+cargo doc
+
+rm -rf $DOCS_DIR && \
+  mv target/doc $DOCS_DIR
+
+cat << EOF > $DOCS_DIR/index.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0; url=ggbc/index.html" />
+</head>
+<body>
+    You are being redirected to the <a href="ggbc/index.html">documentation page</a>...
+</body>
+</html>
+EOF
+
+git -C $DOCS_DIR init && \
+  git -C $DOCS_DIR remote add origin https://germangb:$TOKEN@github.com/germangb/ggb-lang.git && \
+  git -C $DOCS_DIR checkout -b gh-pages && \
+  git -C $DOCS_DIR add -A && \
+  git -C $DOCS_DIR commit -m "Publish docs" > /dev/null && \
+  git -C $DOCS_DIR push origin gh-pages --force --quiet
