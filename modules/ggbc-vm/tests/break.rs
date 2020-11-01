0c124fc01453c06d8ca7461ad5c2f8d2792ebf34
german gomez <germangb42@gmail.com>
2020-11-01 21:22:10 +0100

    ignore deref test

---
+++ modules/ggbc-vm/tests/break.rs
@@ -0,0 +1,6 @@
+mod utils;
+
+#[test]
+fn break_() {
+    utils::run(include_str!("programs/break.ggb"));
+}
--- modules/ggbc-vm/tests/deref.rs
+++ modules/ggbc-vm/tests/deref.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 #[test]
+#[ignore]
 fn loop_() {
     let memory = utils::run(include_str!("programs/deref.ggb"));
     assert_eq!(&[1, 2, 3], &memory.static_[..3])
