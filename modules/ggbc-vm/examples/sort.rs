d1b0887a915289c5636bfd469e77c624bd7409ce
german gomez <germangb42@gmail.com>
2020-11-11 20:57:02 +0100

    sort example & test

---
+++ modules/ggbc-vm/examples/sort.rs
@@ -0,0 +1,5 @@
+mod utils;
+
+fn main() {
+    utils::run(include_str!("../tests/programs/sort.ggb"), None)
+}
---
+++ modules/ggbc-vm/tests/string.rs
@@ -0,0 +1,7 @@
+mod utils;
+
+#[test]
+#[ignore]
+fn break_() {
+    utils::run(include_str!("programs/string.ggb"));
+}
