b6430da98611e2bbb6c72536afcbaa4bf7ec6a63
german gomez <germangb42@gmail.com>
2020-11-24 23:43:46 +0100

    rust compilation target as an interpreter

--- ggbc/examples/rust.rs
+++ ggbc/examples/rust.rs
@@ -4,5 +4,11 @@ fn main() {
     let rust =
         ggbc::compile::<Rust>(include_str!("../../vm/tests/programs/recursion.ggb")).unwrap();
 
-    println!("{}", rust);
+    std::fs::write("out.rs", rust).unwrap();
+    std::process::Command::new("rustc").args(&["out.rs"])
+                                       .spawn()
+                                       .unwrap()
+                                       .wait()
+                                       .unwrap();
+    std::fs::remove_file("out.rs").unwrap();
 }
