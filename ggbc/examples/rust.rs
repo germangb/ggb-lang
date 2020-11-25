9e3a3bb9ffa2ff3e1543873a90b1c0f78f83eb19
german gomez <germangb42@gmail.com>
2020-11-25 23:12:13 +0100

    initial (untested) implementation of rust compilation target

--- ggbc/examples/rust.rs
+++ ggbc/examples/rust.rs
@@ -1,14 +1,21 @@
 use ggbc::target::Rust;
+use std::{
+    io,
+    io::{BufWriter, Cursor},
+};
 
 fn main() {
     let rust =
         ggbc::compile::<Rust>(include_str!("../../vm/tests/programs/recursion.ggb")).unwrap();
 
-    std::fs::write("out.rs", rust).unwrap();
-    std::process::Command::new("rustc").args(&["out.rs"])
-                                       .spawn()
-                                       .unwrap()
-                                       .wait()
-                                       .unwrap();
-    std::fs::remove_file("out.rs").unwrap();
+    // std::fs::write("out.rs", rust).unwrap();
+    // std::process::Command::new("rustc").args(&["out.rs"])
+    //                                    .spawn()
+    //                                    .unwrap()
+    //                                    .wait()
+    //                                    .unwrap();
+    // std::fs::remove_file("out.rs").unwrap();
+
+    std::io::copy(&mut Cursor::new(rust.as_bytes()),
+                  &mut BufWriter::new(io::stdout())).unwrap();
 }
--- ggbc/src/target/rust.rs
+++ ggbc/src/target/rust.rs
@@ -14,6 +14,13 @@ use std::io::Write;
 #[warn(clippy::empty_enum)]
 pub enum Rust {}
 
+macro_rules! output {
+    ($expr:expr) => {};
+    ($expr:expr, $($args:tt)*) => {
+        write!($expr, $($args)*)
+    }
+}
+
 impl Target for Rust {
     type ByteOrder = NativeEndian;
     type Output = String;
@@ -23,19 +30,23 @@ impl Target for Rust {
     fn codegen(ir: &Ir<Self::ByteOrder>) -> Result<Self::Output, Self::Error> {
         let mut output = Vec::new();
 
-        writeln!(&mut output,
-                 "static CONST:[u8;{}] = {:?};",
-                 ir.const_.len(),
-                 ir.const_)?;
-        writeln!(&mut output,
-                 "static mut STATIC:[u8;{}] = [0; {}];",
-                 ir.static_alloc, ir.static_alloc)?;
-        writeln!(&mut output, "static mut REGISTERS:[u8;16] = [0;16];")?;
+        output!(&mut output,
+                "static CONST:[u8;{}] = {:?};",
+                ir.const_.len(),
+                ir.const_)?;
+        output!(&mut output,
+                "static mut STATIC:[u8;{}] = [0; {}];",
+                ir.static_alloc,
+                ir.static_alloc)?;
+        output!(&mut output, "static mut REGISTERS:[u8;16] = [0;16];")?;
+        output!(&mut output, "static mut RETURN:[u8;{}] = [0; {}];", 16, 16)?;
 
-        for routine in ir.routines.iter() {
-            codegen_routine(&ir.routines, &mut output, routine)?;
+        for (i, routine) in ir.routines.iter().enumerate() {
+            codegen_routine(&ir.routines, &mut output, (i, routine))?;
         }
-        writeln!(&mut output, "fn main() {{ unsafe {{ _main([]); }} }}")?;
+        output!(&mut output,
+                "fn main(){{unsafe{{ _{main}([]);}}}}",
+                main = ir.handlers.main)?;
         Ok(String::from_utf8(output).unwrap())
     }
 }
@@ -43,37 +54,35 @@ impl Target for Rust {
 #[warn(unused)]
 fn codegen_routine(routines: &[Routine],
                    output: &mut Vec<u8>,
-                   routine: &Routine)
+                   (i, routine): (usize, &Routine))
                    -> Result<(), std::io::Error> {
-    writeln!(output,
-             "unsafe fn _{}(args:[u8;{}])->[u8;{}] {{",
-             routine.debug_name.as_ref().unwrap(),
-             routine.args_size,
-             routine.return_size)?;
-    writeln!(output, "    let mut RETURN=[0;256];")?;
-    writeln!(output, "    let mut STACK=[0;256];")?;
+    output!(output,
+            "unsafe fn _{}(args:[u8;{}])->[u8;{}] {{",
+            //routine.debug_name.as_ref().unwrap(),
+            i,
+            routine.args_size,
+            routine.return_size)?;
+    output!(output, "let mut STACK=[0;256];")?;
     for i in 0..routine.args_size {
-        writeln!(output, "    STACK[{}] = args[{}];", i, i)?;
+        output!(output, "STACK[{}] = args[{}];", i, i)?;
     }
-    writeln!(output, "    let mut ret=[0;{}];", routine.return_size)?;
-    writeln!(output, "    let mut pc = 0;")?;
-    writeln!(output, "    loop {{")?;
-    writeln!(output, "        match pc {{")?;
+    output!(output, "let mut pc=0;")?;
+    output!(output, "loop {{")?;
+    output!(output, "match pc{{")?;
     for (i, statement) in routine.statements.iter().enumerate() {
-        write!(output, "            {} => ", i)?;
-        //write!(output, "{{")?;
-        codegen_statement(routines, output, statement)?;
-        //write!(output, "}}")?;
-        write!(output, ",")?;
-        //write!(output, " // {}", statement.display())?;
-        writeln!(output);
+        output!(output, "{}=>", i)?;
+        output!(output, "{{")?;
+        codegen_statement(routines, output, statement, routine)?;
+        output!(output, "}}")?;
+        output!(output, ",")?;
+        //output!(output, "/*{}*/", statement.display())?;
+        output!(output);
     }
-    writeln!(output, "            _ => panic!(),")?;
-    writeln!(output, "        }}")?;
-    writeln!(output, "        pc += 1;")?;
-    writeln!(output, "    }}")?;
-    writeln!(output, "    ret")?;
-    writeln!(output, "}}")?;
+    output!(output, "_=>panic!(),")?;
+    output!(output, "}}")?;
+    output!(output, "pc += 1;")?;
+    output!(output, "}}")?;
+    output!(output, "}}")?;
     Ok(())
 }
 
@@ -81,93 +90,106 @@ fn codegen_routine(routines: &[Routine],
 #[rustfmt::skip]
 fn codegen_statement(routines: &[Routine],
                      output: &mut Vec<u8>,
-                     statement: &Statement)
+                     statement: &Statement,
+                     routine: &Routine)
                      -> Result<(), std::io::Error> {
     use Statement::*;
     match statement {
-        Nop(_) => write!(output, "{{}}")?,
-        Stop => write!(output, "std::process::exit(0)")?,
-        Ld { source, destination, } => write!(output, "{}={}", dest(destination), src(source))?,
-        Inc { source, destination, } => write!(output, "{}=({} as u8).wrapping_add(1u8)", dest(destination), src(source))?,
-        Dec { source, destination, } => write!(output, "{}=({} as u8).wrapping_sub(1u8)", dest(destination), src(source))?,
-        Add { destination, left, right, } => write!(output, "{}=({} as u8).wrapping_add({} as u8)", dest(destination), src(left), src(right))?,
-        Sub { destination, left, right, } => write!(output, "{}=({} as u8).wrapping_sub({} as u8)", dest(destination), src(left), src(right))?,
-        And { destination, left, right, } => write!(output, "{}={}&{}", dest(destination), src(left), src(right))?,
-        Xor { destination, left, right, } => write!(output, "{}={}^{}", dest(destination), src(left), src(right))?,
-        Or { destination, left, right, } => write!(output, "{}={}|{}", dest(destination), src(left), src(right))?,
-        LeftShift { destination, left, right, } => write!(output, "{}={}<<{}", dest(destination), src(left), src(right))?,
-        RightShift { destination, left, right, } => write!(output, "{}={}>>{}", dest(destination), src(left), src(right))?,
-        Mul { destination, left, right, } => write!(output, "{}={}.wrapping_mul({})", dest(destination), src(left), src(right))?,
-        Div { destination, left, right, } => write!(output, "{}={}.wrapping_div({})", dest(destination), src(left), src(right))?,
-        Rem { destination, left, right, } => write!(output, "{}={}.wrapping_rem({})", dest(destination), src(left), src(right))?,
-        Eq { destination, left, right, } => write!(output, "{}=if {}=={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
-        NotEq { destination, left, right, } => write!(output, "{}=if {}!={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
-        Greater { destination, left, right, } => write!(output, "{}=if {}>{}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
-        GreaterEq { destination, left, right, } => write!(output, "{}=if {}>={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
-        Less { destination, left, right, } => write!(output, "{}=if {}<{}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
-        LessEq { destination, left, right, } => write!(output, "{}=if {}<={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
+        Nop(_) => output!(output, "{{}}")?,
+        Stop => output!(output, "std::process::exit(0)")?,
+        Ld { source, destination, } => output!(output, "{}={}", dest(destination), src(source))?,
+        Inc { source, destination, } => output!(output, "{}=({} as u8).wrapping_add(1u8)", dest(destination), src(source))?,
+        Dec { source, destination, } => output!(output, "{}=({} as u8).wrapping_sub(1u8)", dest(destination), src(source))?,
+        Add { destination, left, right, } => output!(output, "{}=({} as u8).wrapping_add({} as u8)", dest(destination), src(left), src(right))?,
+        Sub { destination, left, right, } => output!(output, "{}=({} as u8).wrapping_sub({} as u8)", dest(destination), src(left), src(right))?,
+        And { destination, left, right, } => output!(output, "{}={}&{}", dest(destination), src(left), src(right))?,
+        Xor { destination, left, right, } => output!(output, "{}={}^{}", dest(destination), src(left), src(right))?,
+        Or { destination, left, right, } => output!(output, "{}={}|{}", dest(destination), src(left), src(right))?,
+        LeftShift { destination, left, right, } => output!(output, "{}={}<<{}", dest(destination), src(left), src(right))?,
+        RightShift { destination, left, right, } => output!(output, "{}={}>>{}", dest(destination), src(left), src(right))?,
+        Mul { destination, left, right, } => output!(output, "{}={}.wrapping_mul({})", dest(destination), src(left), src(right))?,
+        Div { destination, left, right, } => output!(output, "{}={}.wrapping_div({})", dest(destination), src(left), src(right))?,
+        Rem { destination, left, right, } => output!(output, "{}={}.wrapping_rem({})", dest(destination), src(left), src(right))?,
+        Eq { destination, left, right, } => output!(output, "{}=if {}=={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
+        NotEq { destination, left, right, } => output!(output, "{}=if {}!={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
+        Greater { destination, left, right, } => output!(output, "{}=if {}>{}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
+        GreaterEq { destination, left, right, } => output!(output, "{}=if {}>={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
+        Less { destination, left, right, } => output!(output, "{}=if {}<{}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
+        LessEq { destination, left, right, } => output!(output, "{}=if {}<={}{{1}}else{{0}}", dest(destination), src(left), src(right))?,
         Jmp { location: Location::Relative(r), } => {
             if *r >= 0 {
-                write!(output, "pc += {}", r)?
+                output!(output, "pc+={}", r)?
             } else {
-                write!(output, "pc -= {}", -r)?
+                output!(output, "pc-={}", -r)?
             }
         }
         JmpCmp { location: Location::Relative(r),
-                 source, } => write!(output, "{{}}")?,
+                 source, } => {
+            if *r >= 0 {
+                output!(output, "if {}!=0{{pc+={}}}", src(source), r)?
+            } else {
+                output!(output, "if {}!=0{{pc+={}}}", src(source), r)?
+            }
+        },
         JmpCmpNot { location: Location::Relative(r),
-                    source, } => write!(output, "{{}}")?,
-        Call { routine, range } => {
-            let routine = &routines[*routine];
-            write!(output, "{{")?;
-            write!(output,
-                   "let args:[u8;{}] = [0; {}];",
-                   routine.args_size, routine.args_size)?;
-            write!(output, "}}")?;
+                    source, } => {
+            if *r >= 0 {
+                output!(output, "if {}==0{{pc+={}}}", src(source), r)?
+            } else {
+                output!(output, "if {}==0{{pc-={}}}", src(source), -r)?
+            }
         }
-        Ret => write!(output, "return ret")?,
-        _ => write!(output, "unimplemented!()")?,
+        Call { routine, range } => {
+            let routine_ = &routines[*routine];
+            output!(output, "{{")?;
+            output!(output,
+                   "let mut args:[u8;{}]=[0;{}];",
+                   routine_.args_size, routine_.args_size)?;
+            for (i, offset) in range.clone().enumerate() {
+                output!(output, "args[{}]=STACK[{}];", i, offset)?;
+            }
+            output!(output, "let ret=_{}(args);", routine)?;
+            for i in 0..routine_.return_size {
+                output!(output, "RETURN[{}]=ret[{}];", i, i)?;
+            }
+            output!(output, "}}")?;
+        }
+        Ret => {
+            output!(output, "let mut ret=[0;{}];", routine.return_size);
+            for i in 0..routine.return_size {
+                output!(output, "ret[{}]=RETURN[{}];", i, i)?;
+            }
+            output!(output, "return ret")?
+        },
+        _ => output!(output, "unimplemented!()")?,
     }
     Ok(())
 }
 
-#[warn(unused)]
 fn dest(destination: &Destination) -> String {
-    use Destination as D;
     match destination {
-        Destination::Pointer { base, offset } => {
-            let offset = offset.as_ref()
-                               .map(|s| src(s))
-                               .unwrap_or_else(|| "0".to_string());
-            match base {
-                Pointer::Absolute(a) => todo!(),
-                Pointer::Static(a) => format!("STATIC[{}+{} as usize]", a, offset),
-                Pointer::Const(a) => format!("CONST[{}+{} as usize]", a, offset),
-                Pointer::Stack(a) => format!("STACK[{}+{} as usize]", a, offset),
-                Pointer::Return(a) => format!("ret[{}+{} as usize]", a, offset),
-            }
-        }
+        Destination::Pointer { base, offset } => pointer(base, offset),
         Destination::Register(register) => format!("REGISTERS[{}]", register),
     }
 }
 
-#[warn(unused)]
 fn src(source: &Source<u8>) -> String {
-    use Source as S;
     match source {
-        S::Pointer { base, offset } => {
-            let offset = offset.as_ref()
-                               .map(|s| src(s))
-                               .unwrap_or_else(|| "0".to_string());
-            match base {
-                Pointer::Absolute(a) => todo!(),
-                Pointer::Static(a) => format!("STATIC[{}+{} as usize]", a, offset),
-                Pointer::Const(a) => format!("CONST[{}+{} as usize]", a, offset),
-                Pointer::Stack(a) => format!("STACK[{}+{} as usize]", a, offset),
-                Pointer::Return(a) => format!("RETURN[{}+{} as usize]", a, offset),
-            }
-        }
-        S::Register(register) => format!("REGISTERS[{}]", register),
-        S::Literal(literal) => format!("{}", literal),
+        Source::Pointer { base, offset } => pointer(base, offset),
+        Source::Register(register) => format!("REGISTERS[{}]", register),
+        Source::Literal(literal) => format!("{}", literal),
+    }
+}
+
+fn pointer(base: &Pointer, offset: &Option<Box<Source<u8>>>) -> String {
+    let offset = offset.as_ref()
+                       .map(|s| src(s))
+                       .unwrap_or_else(|| "0".to_string());
+    match base {
+        Pointer::Absolute(_) => todo!(),
+        Pointer::Static(a) => format!("STATIC[{}+{} as usize]", a, offset),
+        Pointer::Const(a) => format!("CONST[{}+{} as usize]", a, offset),
+        Pointer::Stack(a) => format!("STACK[{}+{} as usize]", a, offset),
+        Pointer::Return(a) => format!("RETURN[{}+{} as usize]", a, offset),
     }
 }
