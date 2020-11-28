7820978ecdcb3657a1991389c0fbc22c13006332
german gomez <germangb42@gmail.com>
2020-11-28 13:09:21 +0100

    static test with functions

--- ggbc/tests/static.rs
+++ ggbc/tests/static.rs
@@ -42,3 +42,38 @@ fn test_static() {
                  (0..=15).collect::<Vec<_>>().as_ref(),
     )
 }
+
+#[test]
+fn test_static_fn() {
+    _test_static(
+                 r#"
+    static s0:u8 (= s0 0)
+    static s1:u8 (= s1 1)
+    static s2:u8 (= s2 2)
+    fn foo() {
+        static s3:u8 (= s3 3)
+        static s4:u8 (= s4 4)
+        static s5:u8 (= s5 5)
+        {
+            static s6:u8 (= s6 6)
+            static s7:u8 (= s7 7)
+            {
+                static s8:u8 (= s8 8)
+            }
+            static s9:u8 (= s9 9)
+        }
+        static s10:u8 (= s10 10)
+    }
+    static s11:u8 (= s11 11)
+    fn bar(){
+        static s12:u8 (= s12 12)
+        static s13:u8 (= s13 13)
+        {
+            static s14:u8 (= s14 14)
+        }
+        static s15:u8 (= s15 15)
+    }
+    "#,
+                 (0..=15).collect::<Vec<_>>().as_ref(),
+    )
+}
